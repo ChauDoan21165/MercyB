01c0a2db76202ab2c18899efcda86c693cbd08f2
Chau Doan <cd12536@gmail.com>
2025-12-13 04:08:52 +0700

    Create AppRoutes.tsx

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,10 @@
+// src/routes/AppRoutes.tsx
+import { Routes, Route } from "react-router-dom";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Mercy Blade</div>} />
+    </Routes>
+  );
+}
