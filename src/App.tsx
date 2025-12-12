07fd251c2a26ee6c39b90e25fb960d4ea9d49c9c
Chau Doan <cd12536@gmail.com>
2025-12-12 20:05:02 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 // src/App.tsx
-import React from "react";
-import AppRouter from "./router/AppRouter";
+import AppRouter from "./AppRouter";
 
-function App() {
+export default function App() {
   return <AppRouter />;
 }
-
-export default App;
