d3c33cea783bbce7d1dfacdca4d797c37126884d
Chau Doan <cd12536@gmail.com>
2025-12-13 12:31:40 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import Home from "@/pages/Home";
+import Homepage from "@/pages/Homepage";
 import RoomGrid from "@/pages/RoomGrid";
 
 import { LowDataModeProvider } from "@/contexts/LowDataModeContext";
