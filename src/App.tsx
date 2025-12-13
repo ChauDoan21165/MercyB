cac140bf5eeea40df343d87f3abb31b814d3b488
Chau Doan <cd12536@gmail.com>
2025-12-13 14:24:48 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// FILE: src/App.tsx
+// VERSION: 2025-12-13 v1 (router reset)
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import RoomGrid from "@/pages/RoomGrid";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<RoomGrid />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        {/* Home FIRST */}
+        <Route path="/" element={<Home />} />
+
+        {/* Rooms */}
+        <Route path="/rooms" element={<RoomGrid />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
