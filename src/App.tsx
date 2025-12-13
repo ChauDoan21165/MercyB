0733d27c4aee6e8a7db36ff553362844dada8ab6
Chau Doan <cd12536@gmail.com>
2025-12-13 12:14:15 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { LowDataModeProvider } from "@/contexts/LowDataModeContext";
+import Home from "@/pages/Home";
 import RoomGrid from "@/pages/RoomGrid";
+
+import { LowDataModeProvider } from "@/contexts/LowDataModeContext";
 
 const queryClient = new QueryClient();
 
-export default function App() {
+function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <LowDataModeProvider>
-        <Routes>
-          <Route path="/" element={<RoomGrid />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            {/* HOME */}
+            <Route path="/" element={<Home />} />
+
+            {/* ROOMS */}
+            <Route path="/rooms" element={<RoomGrid />} />
+          </Routes>
+        </BrowserRouter>
       </LowDataModeProvider>
     </QueryClientProvider>
   );
 }
+
+export default App;
