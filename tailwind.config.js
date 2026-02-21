1cda778fd604db893c1f1e50b750d83dd947fdbf
ChauDoan21165 <REAL_ID+ChauDoan21165@users.noreply.github.com>
2026-02-20 18:47:54 -0700

    Enable Tailwind build pipeline (v3) + index.css directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
