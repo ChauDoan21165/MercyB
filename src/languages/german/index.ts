24f41e4a1a16325e369edca89b7b4f68afa0117d
Chau Doan <239713933+ChauDoan21165@users.noreply.github.com>
2026-06-21 11:50:24 -0600

    Add language index barrels

---
+++ src/languages/german/index.ts
@@ -0,0 +1,7 @@
+import { lessons as a2Lessons } from "./lessons-a2";
+import { lessons as b1Lessons } from "./lessons-b1";
+import { lessons as b2Lessons } from "./lessons-b2";
+import { lessons as c1Lessons } from "./lessons-c1";
+import { lessons as c2Lessons } from "./lessons-c2";
+
+export { a2Lessons, b1Lessons, b2Lessons, c1Lessons, c2Lessons };
